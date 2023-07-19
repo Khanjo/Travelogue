776ffa7c479c1a234bc37d1c64d849bb8e5bd1d8
Joshua Khan <Khanjo@gmail.com>
2023-07-19 16:36:40 -0700

    add controllers, models, views, and methods for creating and deleting entries

--- PlacesBeen/Controllers/PlacesController.cs
+++ PlacesBeen/Controllers/PlacesController.cs
@@ -6,6 +6,38 @@ namespace PlacesBeen.Controllers
 {
     public class PlacesController : Controller
     {
+        [HttpGet("/places")]
+        public ActionResult Index()
+        {
+            List<Place> allPlaces = Place.GetAll();
+            return View(allPlaces);
+        }
 
+        [HttpGet("/places/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost("/places")]
+        public ActionResult Create(string city, string country, string journal)
+        {
+            Place myplace = new Place(city, country, journal);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("/places/{id}")]
+        public ActionResult Show(int id)
+        {
+            Place foundItem = Place.Find(id);
+            return View(foundItem);
+        }
+
+        [HttpPost("/places/delete")]
+        public ActionResult DeletePlace(int id)
+        {
+            Place.Delete(id);
+            return View();
+        }
     }
 }
--- PlacesBeen/Models/Places.cs
+++ PlacesBeen/Models/Places.cs
@@ -4,15 +4,31 @@ namespace PlacesBeen.Models
 {
     public class Place
     {
-        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Journal { get; set; }
         public int Id { get; }
         private static List<Place> _instances = new List<Place> { };
 
-        public Place(string name)
+        public Place(string city, string country, string journal)
         {
-            Name = name;
+            City = city;
+            Country = country;
+            Journal = journal;
             _instances.Add(this);
             Id = _instances.Count;
+        }
+        public static List<Place> GetAll()
+        {
+            return _instances;
+        }
+        public static Place Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
+        public static void Delete(int id)
+        {
+            _instances.RemoveAt(id - 1);
         }
     }
 }
